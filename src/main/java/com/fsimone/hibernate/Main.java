a1ecdf3dd3859d6e69040358bd46768ebfe57d96
AF06698 <francesco.simone@it.experis.com>
2020-02-20 16:33:27 +0100

    fix application.properties with placeholder

--- src/main/java/com/fsimone/hibernate/Main.java
+++ src/main/java/com/fsimone/hibernate/Main.java
@@ -1,6 +1,7 @@
 package com.fsimone.hibernate;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,13 +12,17 @@ import org.springframework.context.annotation.Bean;
 
 
 /*
-With the release of Hibernate 5.2 the SessionFactory extends the EntityManagerFactoryinterface. This leads to the SessionFactory also being an EntityManagerFactory.
+With the release of Hibernate 5.2 the SessionFactory extends the EntityManagerFactoryinterface.
+This leads to the SessionFactory also being an EntityManagerFactory.
 In previous hibernate releases this wasn’t the case.
-The easy solution is to downgrade the hibernate version to a version < 5.2 as there is no solution for Spring 5.0 (there will be in Spring 5.1).
-Or probably even better don’t use the plain SessionFactory and let Spring Boot autoconfigure the EntityManagerFactory (done by default if hibernate is detected) and use that instead of plain Hibernate.
+The easy solution is to downgrade the hibernate version to a version < 5.2 as there is no solution for Spring 5.0 
+(there will be in Spring 5.1).
+Or probably even better don’t use the plain SessionFactory and let Spring Boot autoconfigure the EntityManagerFactory
+(done by default if hibernate is detected) and use that instead of plain Hibernate.
 The solution is:
-I added a exclude = HibernateJpaAutoConfiguration.classproperty to @SpringBootApplicationannotation.
+Add property    exclude = HibernateJpaAutoConfiguration.class  to   @SpringBootApplicationannotation.
 */
+
 //@SpringBootApplication
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class Main extends SpringBootServletInitializer {
@@ -40,9 +45,9 @@ public class Main extends SpringBootServletInitializer {
         return new ApplicationConfig().setApplicationContext(context);
     }
 
-//	public static void main(String[] args) {
-//		  SpringApplication.run(Main.class, args);
-//	}
+	public static void main(String[] args) {
+		  SpringApplication.run(Main.class, args);
+	}
 
 
 }
